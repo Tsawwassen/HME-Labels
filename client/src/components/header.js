2b752f64518980366b8ae90807b42f73c9e15e01
Mitchell <mitchell.rian.smith@gmail.com>
2021-12-05 11:11:13 -0800

    Add basic bootstrap navbar

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+import {Navbar, Container, Nav} from 'react-bootstrap'
 
 class Header extends Component {
-    constructor(props){
-        super(props);
-        this.state = {};
-    }
+  constructor(props){
+    super(props);
+    this.state = {
 
-    render() {
-        return (
-            <h1> hello </h1>
-    
-        );
     };
+  }
+
+  render() {
+    return (
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand >Shelf Label</Navbar.Brand>
+            <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    );
+  };
 }
   export default Header;
   
