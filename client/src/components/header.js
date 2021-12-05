3199afbd33e32e089a1b480763723087f927ab9a
Mitchell <mitchell.rian.smith@gmail.com>
2021-12-05 09:58:42 -0800

    Updated local node and npm

---
+++ client/src/components/header.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+  	constructor(props){
+	    super(props);
+	    this.state = {
+	    	
+
+    	};
+
+    	//this.handleButton = this.handleButton.bind(this);
+    	//this.checkForDuplicates = this.checkForDuplicates.bind(this);
+  	}
+
+  	componentDidMount(){
+ 
+  	}
+
+  
+
+  	handleButton(event){
+  		
+  	}
+
+
+  	
+  render () {
+  
+
+    return (
+        <h1> Header</h1>
+    );
+  }
+}
+
+export default Header;
