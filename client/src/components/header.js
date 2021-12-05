2a889fef11f7856f689ab7bef0e3cd580b47187d
Mitchell <mitchell.rian.smith@gmail.com>
2021-12-05 10:56:53 -0800

    Add components folder to project with basic header comp

---
+++ client/src/components/header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+
+class Header extends Component {
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <h1> hello </h1>
+    
+        );
+    };
+}
+  export default Header;
+  
