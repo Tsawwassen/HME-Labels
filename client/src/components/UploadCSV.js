e8ec497e2f4738daedb1d43b7221850f8f324803
Mitchell <mitchell.rian.smith@gmail.com>
2021-12-05 17:14:21 -0800

    Add function to get file path from user

--- client/src/components/UploadCSV.js
+++ client/src/components/UploadCSV.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-import { Col, Container, Row, Form } from 'react-bootstrap';
+import { Col, Container, Row, Form, Button } from 'react-bootstrap';
 
 class UploadCSV extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      filePath:"",
+      fileData:""
     };
+
+    this.fileOnChange = this.fileOnChange.bind(this);
+    this.buttonClicked = this.buttonClicked.bind(this);
+  }
+
+  fileOnChange(event){
+    this.setState({filePath: event.target.value});
+  }
+
+  buttonClicked(){
+    console.log(this.state.filePath);
+    console.log("button clicked");
+
   }
 
   render() {
@@ -16,9 +30,10 @@ class UploadCSV extends Component {
                 <Col>
                 <Form>
                     <Form.Group controlId="formFile" className="mb-3">
-                        <Form.Label>Default file input example</Form.Label>
-                        <Form.Control type="file" />
+                        <Form.Label>Input CSV file</Form.Label>
+                        <Form.Control type="file" onChange={this.fileOnChange} value={this.state.filePath} />
                     </Form.Group>
+                    <Button variant="primary" type="button" onClick={this.buttonClicked}>Submit</Button>
                 </Form>
                 </Col>
             </Row>
