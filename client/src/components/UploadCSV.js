ab87ab81c25e3b97b57d623b382820553e882547
Mitchell <mitchell.rian.smith@gmail.com>
2021-12-09 09:42:48 -0800

    Receive file from user and parse as text

--- client/src/components/UploadCSV.js
+++ client/src/components/UploadCSV.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
 import { Col, Container, Row, Form, Button } from 'react-bootstrap';
+let fileReader = new FileReader();
 
 class UploadCSV extends Component {
+  
   constructor(props){
     super(props);
     this.state = {
-      filePath:"",
-      fileData:""
+      filePath: {},
+      fileData: ""
     };
 
     this.fileOnChange = this.fileOnChange.bind(this);
     this.buttonClicked = this.buttonClicked.bind(this);
+    this.handleFileRead = this.handleFileRead.bind(this);
   }
+
+  
 
   fileOnChange(event){
-    this.setState({filePath: event.target.value});
+    console.log(`file changed ${event.target.files[0].name}`);
+    this.setState({filePath: event.target.files[0]});
   }
 
-  buttonClicked(){
-    console.log(this.state.filePath);
-    console.log("button clicked");
+  handleFileRead(e)  {
+    const content = fileReader.result;
+    console.log(content)
+    // … do something with the 'content' …
+  };
 
+  buttonClicked(){
+    console.log(`state file patch - ${this.state.filePath}`);
+    console.log("button clicked");
+  
+    
+    fileReader.onloadend = this.handleFileRead;
+    fileReader.readAsText(this.state.filePath);
+    
   }
 
   render() {
@@ -31,7 +47,7 @@ class UploadCSV extends Component {
                 <Form>
                     <Form.Group controlId="formFile" className="mb-3">
                         <Form.Label>Input CSV file</Form.Label>
-                        <Form.Control type="file" onChange={this.fileOnChange} value={this.state.filePath} />
+                        <Form.Control type="file" onChange={this.fileOnChange}  />
                     </Form.Group>
                     <Button variant="primary" type="button" onClick={this.buttonClicked}>Submit</Button>
                 </Form>
