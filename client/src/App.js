d516515e160f7de9e60d8f7a3e72a62f5bc65a9e
Mitchell <mitchell.rian.smith@gmail.com>
2021-12-18 09:08:13 -0800

    Update app component to class object

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,21 @@ import './App.css';
 
 //Component Imports
 import Header from './components/header';
-import UploadCSV from './components/UploadCSV'
+import UploadCSV from './components/UploadCSV';
+import React, { Component } from 'react';
 
 //Bootstrap import
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <UploadCSV />
-    </div>
-  );
+class App extends Component { 
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <UploadCSV />
+      </div>
+    );
+  }
 }
 
 export default App;
