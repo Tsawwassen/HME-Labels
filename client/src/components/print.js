fdb9a37ef0a3278bc8348bb069733d9f32a34c82
Mitchell <mitchell.rian.smith@gmail.com>
2021-12-24 08:20:14 -0800

    Display CSV data on label and display barcode

--- client/src/components/print.js
+++ client/src/components/print.js
@@ -2,6 +2,8 @@ import './print.css';
 
 
 import React, { Component } from 'react';
+var Barcode = require('react-barcode');
+var startCase = require('lodash.startcase');
 
 const  LABELS_PER_ROW = 2; 
 const ROWS_PER_PAGE = 3;
@@ -10,11 +12,49 @@ function Label(props){
   
   let index = props.label + (props.row * LABELS_PER_ROW) + ((props.page * (LABELS_PER_ROW+ROWS_PER_PAGE)) + props.page); 
   //I wonder if this logic works for any value for LABELS_PER_ROW and ROWS_PER_PAGE
+
+  let barcodeOptions = {
+                width: 2,
+                height: 50,
+                format: "CODE128",
+                displayValue: true,
+                fontOptions: "",
+                font: "monospace",
+                textAlign: "center",
+                textPosition: "bottom",
+                textMargin: 2,
+                fontSize: 20,
+                background: "#ffffff",
+                lineColor: "#000000",
+                margin: 10,
+                marginTop: undefined,
+                marginBottom: undefined,
+                marginLeft: undefined,
+                marginRight: undefined
+  };
   
   if(index >= props.table.length) return <div className='Label'></div>; //Return empty label div to avoid array out of bounds error
   
-  return (<div className='label' >
-            <h1>{props.table[index].part_number} </h1>
+  //change header keys here to display correct info from csv file
+  return (<div className='label' > 
+            <table>
+              <tbody>
+                <tr>
+                  <th>{startCase(Object.keys(props.table[0])[0])}</th>
+                  <td colSpan="2">{props.table[index].part_number}</td>
+                </tr>
+                <tr>
+                <th>{startCase(Object.keys(props.table[0])[1])}</th>
+                  <td colSpan="2">{props.table[index].supplier}</td>
+                </tr>
+                <tr>
+                <th>{startCase(Object.keys(props.table[0])[2])}</th>
+                  <td colSpan="2">{props.table[index].description} </td>
+                </tr>
+              </tbody>
+            </table>
+            
+            <Barcode value={props.table[index].part_number} {...barcodeOptions} />
           </div>);
 }
 
